848775561174e809383679a2a3d041d068f21963
ice949 <justineimasiku21@gmail.com>
2023-03-21 11:07:37 +0200

    complete hero section

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+import './About.css';
+
+function About() {
+
+	return (
+		<div className="about-container">
+
+        </div>
+	);
+}
+
+export default About;
