02bf71fdf72a1689d8d682b9927960a50facc682
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2023-07-19 21:57:52 -0700

    b

--- src/components/portal/Library/Library.js
+++ src/components/portal/Library/Library.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import './Dashboard.css'
 import MinNav from '../minNav/MinVav';
+import m1 from "./mathbooks/m1.pdf";
+import m2 from "./mathbooks/m2.pdf";
 
 const Library = () => {
     return (
@@ -8,6 +10,10 @@ const Library = () => {
         <MinNav />
         <div className="portal-page-content">
         <h2>Library</h2>
+        <ul className="quiz-sub-container">
+            <li className="quiz-sub"><a href={m1}>INTRODUCTION TO DIFFERENTIATION</a></li>
+            <li className="quiz-sub"><a href={m2}>ADDITIONAL MATHEMATICS PURE & APPLIED</a></li>
+        </ul>
         </div>
       </div>
     )
