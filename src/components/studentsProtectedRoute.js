d7cb042cea93ceaadee46b2dd10ecdbfc9a7c815
Justine Imasiku <justineimasiku21@gmail.com>
2023-10-16 23:56:46 +0000

    fix some errors made in the protected routes

--- src/components/studentsProtectedRoute.js
+++ src/components/studentsProtectedRoute.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 // import { getAuth } from "firebase/auth";
 import { Navigate, Outlet } from 'react-router-dom';
 
-Navigate;
+// Navigate;
 const StudentsProtectedRoute = () => {
 	const [authenticated, setAuthenticated] = useState(true);
 
